9203e401e937e2c9a185efee619fc44a6027184a
unknown <yiok79@naver.com>
2024-10-08 00:51:00 +0900

    Cors 수정

--- src/main/java/org/ws/liargame/TestController.java
+++ src/main/java/org/ws/liargame/TestController.java
@@ -1,10 +1,13 @@
 package org.ws.liargame;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", methods = RequestMethod.GET)
 public class TestController {
 
     @GetMapping("/api/data")
